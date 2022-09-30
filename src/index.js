9c425177b6c121c441ce2c798e8c76799d2b095d
Amanda Carnevali <amandacarnevali@Amandas-MacBook-Air.local>
2022-09-29 22:01:15 -0300

    adding get,post,put and delete on index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,64 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World!' });
+app.get('/books', (req, res) => {
+    return res.json([
+        "Todas as suas (im)perfeições",
+        "A Garota do Lago",
+        "Sherlock Holmes: O Cão dos Baskerville",
+        "Drácula",
+        "Revolução dos Bichos",
+        "HP Lovecraft: O Horror de Dunwich",
+        "Edgar Allan Poe: O Gato Preto",
+        "O Iluminado",
+        "Uma mulher na escuridão",
+    ]);
+});
+
+app.post('/books', (req, res) => {
+    return res.json([
+        "Todas as suas (im)perfeições",
+        "A Garota do Lago",
+        "Sherlock Holmes: O Cão dos Baskerville",
+        "Drácula",
+        "Revolução dos Bichos",
+        "HP Lovecraft: O Horror de Dunwich",
+        "Edgar Allan Poe: O Gato Preto",
+        "O Iluminado",
+        "Uma mulher na escuridão",
+        "Hobbit",
+    ]);
+});
+
+app.put('/books/:id', (req, res) => {
+    return res.json([
+        "Todas as suas (im)perfeições",
+        "A Garota do Lago",
+        "Sherlock Holmes: O Cão dos Baskerville",
+        "Drácula",
+        "Revolução dos Bichos",
+        "HP Lovecraft: O Horror de Dunwich",
+        "Edgar Allan Poe: O Gato Preto",
+        "O Iluminado",
+        "Harry Potter e a Pedra Filosofal",
+        "Hobbit",
+    ]);
+});
+
+app.delete('/books/:id', (req, res) => {
+    return res.json([
+        "Todas as suas (im)perfeições",
+        "A Garota do Lago",
+        "Sherlock Holmes: O Cão dos Baskerville",
+        "Drácula",
+        "Revolução dos Bichos",
+        "HP Lovecraft: O Horror de Dunwich",
+        "Edgar Allan Poe: O Gato Preto",
+        "Uma mulher na escuridão",
+        "Hobbit",
+    ]);
 });
 
 app.listen(8080)
